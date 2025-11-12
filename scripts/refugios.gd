a7127d7110fd11a3a65ffb3431980fe9519f50da
AlbaDiez10 <benjavilala22@gmail.com>
2025-11-12 16:28:10 -0300

    Entrega, espero este bien

--- scripts/refugios.gd
+++ scripts/refugios.gd
@@ -11,17 +11,25 @@ func _process(delta):
 	pass
 
 
+# Enlace 1: El Campito Refugio
+# Conecta tu botón (ej. $BotonCampito) a esta función
 func _on_texture_button_pressed():
-	pass # Replace with function body.
+	OS.shell_open("https://elcampitorefugio.org/")
 
 
+# Enlace 2: Huellitas Adopciones
+# Conecta tu botón (ej. $BotonHuellitas) a esta función
 func _on_texture_button_2_pressed():
-	pass # Replace with function body.
+	OS.shell_open("https://www.instagram.com/huellitas.adopciones/?hl=es")
 
 
+# Enlace 3: Rescataditos La Plata
+# Conecta tu botón (ej. $BotonRescataditos) a esta función
 func _on_texture_button_3_pressed():
-	pass # Replace with function body.
+	OS.shell_open("https://www.instagram.com/rescataditoslaplata/?hl=es")
 
 
+# Enlace 4: Rescatistas LP
+# Conecta tu botón (ej. $BotonRescatistas) a esta función
 func _on_texture_button_4_pressed():
-	pass # Replace with function body.
+	OS.shell_open("https://www.instagram.com/rescatistaslp/")
